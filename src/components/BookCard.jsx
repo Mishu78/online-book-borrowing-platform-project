edc90cd5338a62a6ae525a06135ef572d3408343
programming hero <meherun2002@gmail.com>
2026-05-02 15:47:09 +0600

    card issue fixed

--- src/components/BookCard.jsx
+++ src/components/BookCard.jsx
@@ -2,18 +2,26 @@ import { Button, Card } from '@heroui/react';
 import Image from 'next/image';
 import React from 'react';
 
-const BookCard = ({book}) => {
+const BookCard = ({ book }) => {
     return (
-       <Card className='border'>
-        <div className='relative w-full aspect-square'>
-            <Image src={book.image_url} fill sizes='(max-width:768px) 100vw, (max-width:1200px) 50vw, 33vw' alt={book.title} className='object-contain rounded-xl'/>         
-        </div>
+        <Card className='border p-4 flex flex-col gap-3'>
+    
+            <div className='relative w-full aspect-[3/4] overflow-hidden rounded-xl'>
+                <Image 
+                    src={book.image_url} 
+                    fill 
+                    sizes='(max-width:768px) 100vw, (max-width:1200px) 50vw, 33vw' 
+                    alt={book.title} 
+                    className='object-cover'
+                />         
+            </div>
 
-         <div>
-<h1  className="font-medium">{book.title}</h1>
-        </div>
-        <Button variant='outline' className={'w-full'}>Details</Button>
-       </Card>
+            <div className='flex-grow'>
+                <h1 className="font-medium line-clamp-2">{book.title}</h1>
+            </div>
+            
+            <Button variant='bordered' className='w-full'>View Details</Button>
+        </Card>
     );
 };
 
